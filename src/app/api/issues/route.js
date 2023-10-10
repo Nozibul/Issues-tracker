329affbe4ef4a8e43719b5becdc652d82f34494c
Nozibul <nozibulislamspi@gmail,com>
2023-10-10 21:38:07 +0600

    create an api for create issue

--- src/app/api/issues/route.js
+++ src/app/api/issues/route.js
@@ -1,14 +1,16 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-
 import { z } from "zod";
 
+
+// create issue schema
 const issueSchema = z.object({
     title: z.string().min(1).max(255),
     description: z.string().min(1)
 })
 
 export async function POST(request) {
+  const prisma = new PrismaClient();
 
   try {
     const body = await request.json();
@@ -17,16 +19,15 @@ export async function POST(request) {
     if(!validation.success) 
     return NextResponse.json(validation.error.errors, {status: 400}) ;
 
-    const prisma = new PrismaClient();
-    const newIssue = await prisma.issue({
+    const newIssue = await prisma.issue.create({
         data: { title: body.title, description: body.description }
     });
-
+     console.log(newIssue);
     return NextResponse.json(newIssue, { status: 201});
 
 
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     return NextResponse.json({ status: "Fail" });
 
   }
